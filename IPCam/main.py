9e9ac8a73b8b5e95be6eba6118f9d46cc94f4f61
Gabriel Viel <g.viel@net4up.com>
2018-03-23 18:26:52 +0100

    upload.ps1

--- IPCam/main.py
+++ IPCam/main.py
@@ -1,8 +1,50 @@
-import os
 import network
+import socket
+import machine
+
+def do_connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('Backoffice', 'back4cyim')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
 
 
 if __name__ == '__main__':
-	wlan = network.WLAN(network.STA_IF) # create station interface
-	wlan.active(True)       # activate the interface
-	print(wlan.scan())
+	do_connect()
+	pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
+
+	html = """<!DOCTYPE html>
+	<html>
+	    <head> <title>ESP8266 Pins</title> </head>
+	    <body> <h1>ESP8266 Pins</h1>
+	        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+	    </body>
+	</html>
+	"""
+
+	addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+	s = socket.socket()
+	s.bind(addr)
+	s.listen(1)
+
+	print('listening on', addr)
+
+	while True:
+	    cl, addr = s.accept()
+	    print('client connected from', addr)
+	    cl_file = cl.makefile('rwb', 0)
+	    while True:
+	        line = cl_file.readline()
+	        if not line or line == b'\r\n':
+	            break
+	    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+	    response = html % '\n'.join(rows)
+	    cl.send(response)
+	    cl.close()
--- IPCam/upload.ps1
+++ IPCam/upload.ps1
@@ -1,7 +1,8 @@
 
 param($port = "COM12", $baud = 115200, [Parameter(Mandatory=$True)]$file)
 
-$delay = 200
+$delay = 100
+$offset = 40
 $port = new-Object System.IO.Ports.SerialPort $port,$baud,None,8,one
 
 # Read source file
@@ -9,7 +10,8 @@ $basename = Split-Path -Path $file -Leaf -Resolve
 $content = [System.IO.File]::ReadAllText( ( Resolve-Path $file ) )
 $Bytes = [System.Text.Encoding]::UTF8.GetBytes($content)
 $EncodedText =[Convert]::ToBase64String($Bytes)
-
+$n = $EncodedText.length
+$i=0
 
 # Open and write destination File
 $port.Open()
@@ -23,11 +25,23 @@ Write-Host $port.ReadExisting()
 $port.WriteLine( "f = open('" + $basename +"', 'wb')" + "`r" )
 start-sleep -m $delay
 Write-Host $port.ReadExisting()
-$port.WriteLine( "f.write(ubinascii.a2b_base64(b'" + $EncodedText + "'))" + "`r" )
+$port.WriteLine( "f.write(ubinascii.a2b_base64(b'")
+start-sleep -m $delay
+Write-Host $port.ReadExisting()
+while( ($i+$offset) -lt $n )
+{
+    $port.Write($EncodedText,$i,$offset)
+    $i += $offset
+    start-sleep -m $delay
+    Write-Host $port.ReadExisting()
+}
+$port.Write($EncodedText,$i,$n-$i)
+start-sleep -m $delay
+Write-Host $port.ReadExisting()
+$port.WriteLine(  "'))" + "`r" )
 start-sleep -m $delay
 Write-Host $port.ReadExisting()
 $port.WriteLine( "f.close()" + "`r" )
 start-sleep -m $delay
 Write-Host $port.ReadExisting()
-start-sleep -m $delay
 $port.Close() 
