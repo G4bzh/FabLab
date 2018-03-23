d2b4dfeda803b2fb42159bd43ac40bcbae2f2133
Gabriel Viel <g.viel@net4up.com>
2018-03-23 19:22:47 +0100

    uploader and web server

--- IPCam/main.py
+++ IPCam/main.py
@@ -1,6 +1,4 @@
 import network
-import socket
-import machine
 
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
@@ -13,38 +11,6 @@ def do_connect():
             pass
     print('network config:', wlan.ifconfig())
 
-
-
 if __name__ == '__main__':
 	do_connect()
-	pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
-
-	html = """<!DOCTYPE html>
-	<html>
-	    <head> <title>ESP8266 Pins</title> </head>
-	    <body> <h1>ESP8266 Pins</h1>
-	        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
-	    </body>
-	</html>
-	"""
-
-	addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
-	s = socket.socket()
-	s.bind(addr)
-	s.listen(1)
-
-	print('listening on', addr)
-
-	while True:
-	    cl, addr = s.accept()
-	    print('client connected from', addr)
-	    cl_file = cl.makefile('rwb', 0)
-	    while True:
-	        line = cl_file.readline()
-	        if not line or line == b'\r\n':
-	            break
-	    rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-	    response = html % '\n'.join(rows)
-	    cl.send(response)
-	    cl.close()
+	# import server
--- IPCam/upload.ps1
+++ IPCam/upload.ps1
@@ -1,8 +1,7 @@
 
 param($port = "COM12", $baud = 115200, [Parameter(Mandatory=$True)]$file)
 
-$delay = 100
-$offset = 40
+$delay = 200
 $port = new-Object System.IO.Ports.SerialPort $port,$baud,None,8,one
 
 # Read source file
@@ -10,7 +9,7 @@ $basename = Split-Path -Path $file -Leaf -Resolve
 $content = [System.IO.File]::ReadAllText( ( Resolve-Path $file ) )
 $Bytes = [System.Text.Encoding]::UTF8.GetBytes($content)
 $EncodedText =[Convert]::ToBase64String($Bytes)
-$n = $EncodedText.length
+
 $i=0
 
 # Open and write destination File
@@ -25,22 +24,16 @@ Write-Host $port.ReadExisting()
 $port.WriteLine( "f = open('" + $basename +"', 'wb')" + "`r" )
 start-sleep -m $delay
 Write-Host $port.ReadExisting()
-$port.WriteLine( "f.write(ubinascii.a2b_base64(b'")
-start-sleep -m $delay
-Write-Host $port.ReadExisting()
-while( ($i+$offset) -lt $n )
+
+$pad = "f.write(ubinascii.a2b_base64(b'" + $EncodedText + "'))" + "`r"
+while( $i -lt $pad.length)
 {
-    $port.Write($EncodedText,$i,$offset)
-    $i += $offset
-    start-sleep -m $delay
-    Write-Host $port.ReadExisting()
+    $port.Write($pad,$i,1)
+    $i+=1
+    start-sleep -m 5
 }
-$port.Write($EncodedText,$i,$n-$i)
-start-sleep -m $delay
 Write-Host $port.ReadExisting()
-$port.WriteLine(  "'))" + "`r" )
-start-sleep -m $delay
-Write-Host $port.ReadExisting()
+
 $port.WriteLine( "f.close()" + "`r" )
 start-sleep -m $delay
 Write-Host $port.ReadExisting()
