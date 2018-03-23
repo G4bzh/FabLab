b08b24a4ddb64b94d8f8156f34c5bd8e8b0ef63f
Gabriel Viel <g.viel@net4up.com>
2018-03-23 17:20:21 +0100

    uploader

--- IPCam/main.py
+++ IPCam/main.py
@@ -1,4 +1,8 @@
 import os
+import network
+
 
 if __name__ == '__main__':
-	print(os.listdir())
+	wlan = network.WLAN(network.STA_IF) # create station interface
+	wlan.active(True)       # activate the interface
+	print(wlan.scan())
--- IPCam/upload.ps1
+++ IPCam/upload.ps1
@@ -5,18 +5,19 @@ param($port = "COM12", $baud = 115200, [Parameter(Mandatory=$True)]$file)
 $port = new-Object System.IO.Ports.SerialPort $port,$baud,None,8,one
 
 # Read source file
-$file = Get-Childitem -path $file
 $basename = Split-Path -Path $file -Leaf -Resolve
+
+$content = [System.IO.File]::ReadAllText( ( Resolve-Path $file ) )
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($content)
+$EncodedText =[Convert]::ToBase64String($Bytes)
 
 # Open and write destination File
 $port.Open()
 $port.WriteLine( "`r" )
 $port.WriteLine( "import os" + "`r" )
-$port.WriteLine( "f = open('" + $basename +"', 'w')" + "`r" )
-foreach( $line in [System.IO.File]::ReadLines($file))
-{
-    $port.WriteLine( "f.write('" + $line + "\n')" + "`r" )
-}
+$port.WriteLine( "import ubinascii" + "`r" )
+$port.WriteLine( "f = open('" + $basename +"', 'wb')" + "`r" )
+$port.WriteLine( "f.write(ubinascii.a2b_base64(b'" + $EncodedText + "'))" + "`r" )
 $port.WriteLine( "f.close()" + "`r" )
 start-sleep -m 50
 $port.Close()
