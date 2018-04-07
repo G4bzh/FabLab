292a5874c8e57ddffd7c10c4d565969d6cb7a812
Gabriel Viel <g.viel@net4up.com>
2018-04-07 16:10:16 +0200

    at communication

--- IPCam/ESPino/ESPino.ino
+++ IPCam/ESPino/ESPino.ino
@@ -1,59 +1,66 @@
+/*
+ * Issue AT+IPR=9600
+ */
+
+
 #include <SoftwareSerial.h>
 
 #define WAIT 100
 
 SoftwareSerial ESPserial(3, 4); // RX | TX
 
-char c = ' ';
 
-
-void sendESP(SoftwareSerial *ss, char* str)
+void sendESP(SoftwareSerial* ss, char* cmd, char* buff, int* sz)
 {
-    int n = strlen(str);
-    int i;
-    
-    for(i=0;i<n;i++)
+  int i,n,k,ok;
+  char c;
+
+  k=0;
+  ok=0;
+
+  /* Send command */
+  ss->println(cmd);
+
+  
+  while(!ok)
+  {
+  
+    if (n=ss->available())
     {
-       ss->write(str[i]);
-       delay(WAIT);
+
+      /* Fill buffer with new data */
+      for(i=0;i<n;i++)
+      {
+        buff[k+i] = ss->read();
+      }
+      /* Update counter */
+      k+=i;
+
+      /* Look for "OK\r" that terminates an AT command response */
+      for(i=0;i<k-2;i++)
+      {
+        if ( (buff[i] == 'O') && (buff[i+1] == 'K') && (buff[i+2] == '\r') )
+        {
+          /* Found ! */
+          ok = 1;
+        }
+      }
+      
     }
     
-    return;
-}
-
-
-int checkESP(SoftwareSerial *ss)
-{
-  int n,i,r;
-  char* str;
-  
-  while ( ! (n=ss->available()) )
-  {
   }
 
-  str = (char*)malloc((n+1)*sizeof(char));
-  if ( str == NULL)
-  {
-    return 0;
-  }
-
-  for(i=0;i<n;i++)
-  {
-    str[i] = ss->read();
-    delay(WAIT);
-  }  
-  str[i]=0;
+  /* Set buffer size */
+  *sz = k;
+  /* Null terminate buffer */
+  buff[k] = 0;
   
-  r = (str[i-2] == '>')?1:0;
-  free(str);
-
-  return r;
-
+  return;
 }
- 
+
 void setup() 
 {
-    Serial.begin(115200);
+    Serial.begin(9600);
 
     while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
@@ -62,7 +69,7 @@ void setup()
     Serial.println("Arduino is ready");
     Serial.println("Remember to select Both NL & CR in the serial monitor");
  
-    ESPserial.begin(115200);  
+    ESPserial.begin(9600);  
       
 
 
@@ -70,15 +77,14 @@ void setup()
  
 void loop()
 {
+  char str[1024];
+  int n;
   
-  sendESP(&ESPserial,"print('Hello')\r");
-  if (checkESP(&ESPserial))
-  {
-    Serial.println("Command successfully sent !");
-  }
-  else
-  {
-    Serial.println("Fail to send command !"); 
-  }
+  sendESP(&ESPserial,"AT+CWLAP", str, &n);
+  delay(WAIT);
+  Serial.print("Got ");
+  Serial.print(n),
+  Serial.println(" bytes :");
+  Serial.println(str);
   
 }
