8021b15d79704b207bc4e9e3ea58cf461d465e3b
Gabriel Viel <g.viel@net4up.com>
2018-04-10 11:02:05 +0200

    WifiCam

--- WifiCam/src/main.cpp
+++ WifiCam/src/main.cpp
@@ -1,8 +1,4 @@
-/*
- * Issue AT+IPR=9600
- */
-
-
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 
 #define WAIT 200
@@ -13,7 +9,6 @@ SoftwareSerial ESPserial(3, 4); // RX | TX
 void sendESP(SoftwareSerial* ss, char* cmd, char* buff, int* sz)
 {
   int i,n,k,ok;
-  char c;
 
   k=0;
   ok=0;
@@ -23,9 +18,9 @@ void sendESP(SoftwareSerial* ss, char* cmd, char* buff, int* sz)
 
   while(!ok)
   {
-    if (n=ss->available())
+    if ( (n=ss->available()) )
     {
-      
+
       /* Fill buffer with new data */
       for(i=0;i<n;i++)
       {
@@ -43,46 +38,46 @@ void sendESP(SoftwareSerial* ss, char* cmd, char* buff, int* sz)
           ok = 1;
         }
       }
-      
+
     }
-    
+
   }
 
   /* Set buffer size */
   *sz = k;
   /* Null terminate buffer */
   buff[k] = 0;
-  
+
   return;
 }
 
-void setup() 
+void setup()
 {
     Serial.begin(9600);
 
     while (!Serial) {
       ; // wait for serial port to connect. Needed for native USB port only
     }
-    
+
     Serial.println("Arduino is ready");
-    Serial.println("Remember to select Both NL & CR in the serial monitor");
- 
-    ESPserial.begin(9600);  
-      
+    Serial.println("Remember to select Both NL & CR in the serial monitor :)");
+
+    ESPserial.begin(9600);
+
 
 
 }
- 
+
 void loop()
 {
   char str[1024];
   int n;
-  
-  sendESP(&ESPserial,"AT+CWLAP", str, &n);
+
+  sendESP(&ESPserial,(char*)"AT+CWLAP", str, &n);
   delay(WAIT);
   Serial.print("Got ");
   Serial.print(n),
   Serial.println(" bytes :");
   Serial.println(str);
-  
+
 }
